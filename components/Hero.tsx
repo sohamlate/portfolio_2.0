6d9bcc5305f984f686a4b0e94b49845c9ec8d31f
sohamlate <sohamslate2004@gmail.com>
2025-01-06 18:28:45 +0530

    main section added

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      
+   
+    </div>
+
+      
+    
+  );
+} 
+
+export default Hero
